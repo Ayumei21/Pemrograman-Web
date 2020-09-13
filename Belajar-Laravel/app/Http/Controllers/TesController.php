a360afe4bdbe1f6fd66c9b462abe8b0cb174ec23
Anugerah Trisna Hadi <anugerahtrisnahadi@gmail.com>
2020-09-14 06:37:50 +0700

    Laravel v8.0.1

---
+++ Belajar-Laravel/app/Http/Controllers/TesController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TesController extends Controller
+{
+    public function show(){
+    	return "test controller success";
+    }
+    public function insert(){
+
+    }
+    public function delete(){
+    	
+    }
+}
--- Belajar-Laravel/routes/web.php
+++ Belajar-Laravel/routes/web.php
@@ -16,3 +16,28 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Closure
+
+route::get('profil',function(){
+	return "ini adalah profil siswa";
+})->name('profil');
+
+// Route name
+
+Route::get('/tesname', function(){
+	return route('profil');
+});
+
+// Parameter
+
+Route::get('/profil/{id}', function($id){
+	return $id;
+});
+
+// Controller
+
+Route::get('/tescontroller', "TesController@show");
+
+// Resource
+Route::resource('siswa','resController');
